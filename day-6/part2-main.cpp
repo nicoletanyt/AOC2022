25b90beff36d28412a5cd05471386afcbaa989b2
nicoletanyt <nicole_tan_yitong@s2021.ssts.edu.sg>
2022-12-09 14:08:41 +0800

    finished day 6 part 2

---
+++ day-6/part2-main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <fstream>
+#include <vector>
+using namespace std;
+
+int main() {
+    ifstream dataFile("testData.txt"); // read file
+    string data;
+    bool marker = false;
+    int charDetectedIndex;
+
+    getline(dataFile, data);
+    for (int i = 0; i < data.length(); i++) {
+        string currentLetter;
+        currentLetter += data[i];
+        for (int k = i + 1; k < i + 14; k++) { //loop through next 4 characters
+            if (!(currentLetter.find(data[k]) != string::npos)) {
+                marker = true;
+                currentLetter += data[k];
+            }
+            else {
+                marker = false;
+                break;
+            }
+        }
+        if (marker) {
+            charDetectedIndex = i + 14;
+            break;
+        }
+        currentLetter = "";
+    }
+
+    cout << charDetectedIndex;
+
+    return 0;
+}
